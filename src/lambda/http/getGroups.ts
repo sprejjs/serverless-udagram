2a2599206dd080faf6686dd52f02d9e9dca50c9d
Vlad Spreys <vlad@spreys.com>
2019-09-23 08:26:11 +1000

    Adding connection to DynamoDB

---
+++ src/lambda/http/getGroups.ts
@@ -0,0 +1,23 @@
+import {APIGatewayProxyEvent, APIGatewayProxyHandler} from "aws-lambda";
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const groupsTable = process.env.GROUPS_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+    console.log('Processing event: ', event);
+
+    const result = await docClient.scan({
+        TableName: groupsTable
+    }).promise();
+
+    const items = result.Items;
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin:': '*'
+        },
+        body: JSON.stringify({items})
+    };
+};
