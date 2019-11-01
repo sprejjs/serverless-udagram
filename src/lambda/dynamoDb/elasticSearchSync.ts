059891f0f7f43aac7ae8088ccfddce047ede1705
Vlad Spreys <vlad@spreys.com>
2019-11-02 10:39:37 +1100

    Processing DynamoDb stream

---
+++ src/lambda/dynamoDb/elasticSearchSync.ts
@@ -0,0 +1,9 @@
+import {DynamoDBStreamEvent, DynamoDBStreamHandler} from "aws-lambda";
+
+export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
+    console.log('Processing events batch from DynamoDB', JSON.stringify(event));
+
+    for (const record of event.Records) {
+        console.log('Processing record', JSON.stringify(record))
+    }
+};
